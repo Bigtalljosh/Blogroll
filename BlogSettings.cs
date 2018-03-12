c11e6a332ca3c5f3b909f0e8123389b535639a62
Josh Dadak <joshdadak@gmail.com>
2018-03-12 13:40:45 +0000

    init

---
+++ BlogSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Blogroll
+{
+    public class BlogSettings
+    {
+        public string Name { get; set; } = "Blogroll";
+        public string Description { get; set; } = "The musings and experiments of a developer.";
+        public string Owner { get; set; } = "Josh Dadak";
+        public int PostsPerPage { get; set; } = 5;
+        public int CommentsCloseAfterDays { get; set; } = 10;
+    }
+}
